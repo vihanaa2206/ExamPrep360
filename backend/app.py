3d57f3701c53464b99943339d8f7935a31790b28
vihanaa2206 <thakkarishaan5@gmail.com>
2026-04-21 12:50:08 +0530

    fix: smtp port 465 ssl

--- backend/app.py
+++ backend/app.py
@@ -38,8 +38,9 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 # ---------------- Mail Config ---------------- #
 app.config["MAIL_SERVER"]         = "smtp.gmail.com"
-app.config["MAIL_PORT"]           = 587
-app.config["MAIL_USE_TLS"]        = True
+app.config["MAIL_PORT"]           = 465
+app.config["MAIL_USE_TLS"]        = False
+app.config["MAIL_USE_SSL"]        = True
 app.config["MAIL_USERNAME"]       = os.environ.get("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"]       = os.environ.get("MAIL_PASSWORD")
 app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_USERNAME")
