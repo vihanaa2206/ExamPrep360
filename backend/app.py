eea7c5cc733c4f26c7983a0b5ac10c80fe0fd7fd
vihanaa2206 <thakkarishaan5@gmail.com>
2026-04-21 12:29:48 +0530

    fix: remove duplicate cors headers

--- backend/app.py
+++ backend/app.py
@@ -32,14 +32,6 @@ CORS(app,
      methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
      allow_headers=["Content-Type", "Authorization"])
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', 'https://exam-prep360.vercel.app')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    response.headers.add('Access-Control-Allow-Credentials', 'true')
-    return response
-
 
 # ---------------- MongoDB Config ---------------- #
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
