4257baac50cdb74d80dfaaa505b73f7964db7361
vihanaa2206 <thakkarishaan5@gmail.com>
2026-04-22 15:25:07 +0530

    fix: correct emailjs service ID

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import API from "../services/api";
 import emailjs from "@emailjs/browser";
 
-const EMAILJS_SERVICE  = "service_jc1snng";
+const EMAILJS_SERVICE = "service_jc1sng";
 const EMAILJS_TEMPLATE = "e7h4mub";
 const EMAILJS_KEY      = "PLmVSfeYnfj4hcrns";
 
